393d08ce59bacd6d600fd155786a603a477796b1
Freddy Rangel <frederick.rangel@gmail.com>
2016-04-14 13:06:26 -0700

    Simplify Warp Drive Controls

--- finished/src/components/warp-drive-controls.jsx
+++ finished/src/components/warp-drive-controls.jsx
@@ -2,29 +2,26 @@ import React           from 'react';
 import EditableElement from './editable-element';
 
 const WarpDriveControls = (p) => {
+  // Everyone knows Warp 10 is impossible...
+  const isValidSpeed  = (speed) => 0 <= speed  && speed < 10;
+
+  // Not very DRY but this is clearer for learning purposes
+  const currentSpeed  = p.speed;
+  const increaseSpeed = () => {if (isValidSpeed(currentSpeed + 1)) p.updateSpeed(currentSpeed + 1)};
+  const decreaseSpeed = () => {if (isValidSpeed(currentSpeed - 1)) p.updateSpeed(currentSpeed - 1)};
+
   return <div className="navigational-controls">
     <h2>Warp Drive</h2>
     <span>WARP:</span>
-    <EditableElement value={p.speed} onEdit={updateSpeed.bind(this, p)}/>
+    <p>{currentSpeed}</p>
     <div className="arrow-controls">
-      <button onClick={incrementSpeed.bind(this, p, 1)}>&#11014;</button>
-      <button onClick={incrementSpeed.bind(this, p, -1)}>&#11015;</button>
+      <button onClick={increaseSpeed}>&#11014;</button>
+      <button onClick={decreaseSpeed}>&#11015;</button>
     </div>
     <div className="engage">
       <button onClick={p.engageWarpDrive}>Engage</button>
     </div>
   </div>
 };
-
-const updateSpeed = (p, speed) =>{
-  const newSpeed = parseFloat(speed, 10);
-  if (0 <= newSpeed && newSpeed < 10) p.updateSpeed(newSpeed);
-}
-
-const incrementSpeed = (p, dSpeed) => {
-  const currentSpeed = p.speed;
-  const newSpeed = Math.floor(currentSpeed + dSpeed);
-  if (0 <= newSpeed && newSpeed < 10) p.updateSpeed(newSpeed);
-}
 
 export default WarpDriveControls;
