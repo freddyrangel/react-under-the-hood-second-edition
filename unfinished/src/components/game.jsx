9b5604485eaa9562239887268f640785cfa4e410
Freddy Rangel <frederick.rangel@gmail.com>
2016-04-13 22:48:20 -0700

    Render ship on star chart

--- unfinished/src/components/game.jsx
+++ unfinished/src/components/game.jsx
@@ -1,11 +1,18 @@
 import React           from 'react';
 import StarChart       from './star-chart';
 import {
-  starData
+  starData,
+  initialShipData
 } from 'lib';
 
 export default class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {ship: initialShipData};
+  }
+
   render() {
-    return <StarChart starData={starData} />
+    return <StarChart starData={starData} ship={this.state.ship}/>
   }
 }
+
--- unfinished/src/components/star-chart.jsx
+++ unfinished/src/components/star-chart.jsx
@@ -1,11 +1,13 @@
 import React            from 'react';
 import Stars            from './stars';
+import StarshipRenderer from './starship-renderer';
 
 const StarChart = (p) => {
   return <div>
     <div className="star-chart">
       <svg width="1000" height="600">
         <Stars starData={p.starData} />
+        <StarshipRenderer ship={p.ship}/>
       </svg>
     </div>
   </div>
