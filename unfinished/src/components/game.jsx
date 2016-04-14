96f1d128fee088147f724999b18f104ad149a092
Freddy Rangel <frederick.rangel@gmail.com>
2016-04-13 23:55:19 -0700

    Ship Info Component

--- unfinished/src/components/game.jsx
+++ unfinished/src/components/game.jsx
@@ -1,5 +1,6 @@
 import React           from 'react';
 import StarChart       from './star-chart';
+import HelmControl     from './helm-control';
 import {
   starData,
   initialShipData
@@ -9,10 +10,31 @@ export default class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {ship: initialShipData};
+    this.getShip           = this.getShip.bind(this);
+    this.updateShip        = this.updateShip.bind(this);
+    this.updateShipInfoKey = this.updateShipInfoKey.bind(this);
+  }
+
+  getShip() {
+    return Object.assign({}, this.state.ship);
+  }
+
+  updateShip(key, value) {
+    const ship = this.getShip();
+    ship[key] = value;
+    this.setState({ship: ship});
+  }
+
+  updateShipInfoKey(key, value) {
+    const info = this.getShip().info;
+    info[key] = value;
+    this.updateShip('info', info);
   }
 
   render() {
-    return <StarChart starData={starData} ship={this.state.ship}/>
+    return <div>
+      <StarChart starData={starData} ship={this.state.ship}/>
+      <HelmControl ship={this.state.ship} updateShipInfoKey={this.updateShipInfoKey} />
+    </div>
   }
 }
-
